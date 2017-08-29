dfce54b657ed75673baec5587139b4210b0e0617
IvVelichkova <night_princess.1986@abv.bg>
2017-08-29 18:57:32 +0300

    -OOPLAB

---
+++ OOP1Lab/1.ReverseNumbersWithAStack/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.ReverseNumbersWithAStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = Console.ReadLine();
+            var values = number.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                
+            var myStack = new Stack<int>(values);
+
+            var reverseStack = new Stack<int>();
+            while (myStack.Count!=0)
+            {
+                Console.Write($"{myStack.Pop()} ");
+               
+            }
+            
+
+
+
+
+            /*// An example string array.
+        string[] values = { "Dot", "Net", "Perls" };
+
+        // Copy an array into a Stack.
+        var stack = new Stack<string>(values);
+
+        // Display the Stack.
+        Console.WriteLine("--- Stack contents ---");
+        foreach (string value in stack)
+        {
+            Console.WriteLine(value);
+        }
+
+        // See if the stack contains "Perls"
+        Console.WriteLine("--- Stack Contains method result ---");
+        bool contains = stack.Contains("Perls");
+        Console.WriteLine(contains); */
+
+
+        }
+    }
+}
---
+++ OOP1Lab/5.HotPotato/5.HotPotato.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var queue = new Queue<string>(input.Split(' '));
+            int cycle = int.Parse(Console.ReadLine());
+            int count = 1;
+            for (int i = 0; i < cycle; i++)
+            {
+                
+            }
+
+            for (int i = 0; i < cycle; i++)
+            {
+                
+
+            }
+
+
+
+        }
+    }
+}
