ca094d728d5fdc04aaa56d935d89591081614c39
IvVelichkova <night_princess.1986@abv.bg>
2017-08-29 19:41:30 +0300

    -BashSoft

---
+++ StoryMode/BashSoft/IOManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    public static class IOManager
+    {
+        public static void TraverseDirectory(string path)
+        {
+            OutputWriter.WriteEmptyLine();
+            int initialIdentation = path.Split('\\').Length;
+            Queue<string> subfolders = new Queue<string>();
+            subfolders.Enqueue(path);
+
+            while (subfolders.Count != 0)
+            {
+
+
+                string currentPath = subfolders.Dequeue();
+                int identation = currentPath.Split('\\').Length - initialIdentation;
+
+
+                foreach (string directoryPath in Directory.GetDirectories(currentPath))
+                {
+                    subfolders.Enqueue(directoryPath);
+                }
+
+                OutputWriter.WriteMessageOnNeLine(string.Format("{0}{1}", new string('-', identation), currentPath));
+            }
+        }
+    }
+}
---
+++ StoryMode/BashSoft/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    class Program
+    {
+       public static void Main(string[] args)
+       {
+           IOManager.TraverseDirectory(@"C:\Users\Iv Velichkova\Desktop\StoryMode\BashSoft");
+            OutputWriter.DisplayException("bla bla");
+
+       }
+    }
+}
