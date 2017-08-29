8a1665818eaef83bcdc4e356255b19af1b6d6bce
IvVelichkova <night_princess.1986@abv.bg>
2017-08-29 19:33:01 +0300

    -Ex

---
+++ FunctionalProgramming/dictirionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace dictirionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            //
+            // Get all words.
+            //
+            string[] uppercaseWords = Regex.Split(sentence, @"\W");
+            //
+            // Get all uppercased words.
+            //
+            var list = new List<string>();
+            foreach (string value in uppercaseWords)
+            {
+                //
+                // Check the word.
+                //
+                if (!string.IsNullOrEmpty(value) &&
+                    char.IsUpper(value[0]))
+                {
+                    list.Add(value);
+                }
+            }
+            //
+            // Write all proper nouns.
+            //
+            foreach (var value in list)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ FunctionalProgramming/first/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    public  enum condition
+    {
+        older=0,
+        younger=1
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Dictionary<string, int> people = new Dictionary<string, int>();
+            for (int i = 0; i < n; i++)
+            {
+                var person = Console.ReadLine().Split().ToArray();
+                people.Add(person[0], int.Parse(person[1]));
+
+
+            }
+            var age = int.Parse(Console.ReadLine());
+            var nameOrAge = Console.ReadLine();
+
+
+        }
+        public static void nameAge(string name)
+        {
+           
+            Console.WriteLine($"{nameAge.}");
+        }
+
+        public static void  nameAge(string name,int userAge,condition,int age,string names)
+        {
+            Console.WriteLine($"{nameAge.Keys} {nameAge.Values}");
+        }
+    }
+}
