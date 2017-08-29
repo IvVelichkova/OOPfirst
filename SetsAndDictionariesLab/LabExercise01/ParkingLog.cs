f6e1d94d14a800190fb8095b35824f4dc1042103
IvVelichkova <night_princess.1986@abv.bg>
2017-08-29 19:22:57 +0300

    -LabEx

---
+++ SetsAndDictionariesLab/LabExercise01/ParkingLog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LabExercise01
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            var input = Console.ReadLine();
+            var parking = new SortedSet<string>();
+
+            while (input !="END")
+            {
+                var parram = Regex.Split(input, ", ");
+                if (parram[0]=="IN")
+                {
+                    parking.Add(parram[1]);
+                }
+                else
+                {
+                    if (parking.Contains(parram[1]))
+                    {
+                        parking.Remove(parram[1]);
+                    }
+                }
+
+
+                input = Console.ReadLine();
+            }
+            if (parking.Count==0)
+            {
+                Console.WriteLine("Parking Lot is Empty");   
+            }
+            else
+            {
+                foreach (var car in parking)
+                {
+
+                    Console.WriteLine(car);
+                }
+               
+            }
+
+        }
+    }
+}
---
+++ SetsAndDictionariesLab/SoftUniParty/Party.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniParty
+{
+   public class Party
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var party = new SortedSet<string>();
+
+
+
+            while (input!="PARTY")
+            {
+                party.Add(input);
+                input = Console.ReadLine();
+            }
+            while (input != "END")
+            {
+                if (party.Contains(input))
+                {
+                    party.Remove(input);
+
+                }
+                input = Console.ReadLine();
+            }
+            if (party.Count!=0)
+            {
+                Console.WriteLine(party.Count);
+                foreach (var p in party)
+                {
+                    Console.WriteLine(p);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
