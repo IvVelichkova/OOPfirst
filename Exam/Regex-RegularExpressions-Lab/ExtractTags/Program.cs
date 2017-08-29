7c6256fe0d732c8a315dc6a5a19cd484a57a1f17
IvVelichkova <night_princess.1986@abv.bg>
2017-08-29 19:37:00 +0300

    -removeError

---
+++ Exam/Regex-RegularExpressions-Lab/ExtractTags/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExtractIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            while (text != "END")
+            {
+                Regex regex = new Regex("<.*?>");
+                MatchCollection mathces = regex.Matches(text);
+
+
+                foreach (var match in mathces)
+                {
+                    Console.Write(match);
+                    Console.WriteLine();
+                }
+                text = Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ Exam/Regex-RegularExpressions-Lab/ValidUsernames/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+
+            while (name!="END")
+            {
+                Regex validation = new Regex("^[\\w\\d-]{3,16}$",RegexOptions.Multiline);
+                MatchCollection match = validation.Matches(name);
+
+
+
+                if (match.Count>0)
+                {
+                    Console.WriteLine("valid");
+                }
+                else
+                {
+                    Console.WriteLine("invalid");
+                }
+                name = Console.ReadLine();
+
+            }
+
+
+        }
+    }
+}
